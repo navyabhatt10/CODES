1f836a18603878bf4efd30c44ce18144db0631e0
Navya Bhatt <75354532+navyabhatt10@users.noreply.github.com>
2021-07-01 23:47:05 +0530

    ques7

---
+++ Day 3/targetArray.cpp
@@ -0,0 +1,19 @@
+/**
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+**/
+class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        vector<int> ans;
+        for(int i=0;i<index.size();i++)
+        {
+            ans.insert(ans.begin()+index[i],nums[i]);
+        }
+            return ans;
+    }
+};
