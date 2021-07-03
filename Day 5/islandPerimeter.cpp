e6819fd169bca194e8bd0f37a76631a3ad5f70c8
Navya Bhatt <75354532+navyabhatt10@users.noreply.github.com>
2021-07-04 01:40:37 +0530

    ques3

---
+++ Day 5/islandPerimeter.cpp
@@ -0,0 +1,35 @@
+/**
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+**/
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        int p=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    p=p+4;
+                if((j-1)>=0 && grid[i][j-1]==1)
+                    p--;
+                 if((j+1)<col && grid[i][j+1]==1)
+                    p--;
+                 if((i-1)>=0 && grid[i-1][j]==1)
+                    p--;
+                 if((i+1)<row && grid[i+1][j]==1)
+                    p--;
+                }
+            }
+        }
+        return p;
+    }
+};
